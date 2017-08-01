99eecc98c0cc19b66e51486a89c512c49e177f3f
Andela Developer <tawakalt.olaniyi@andela.com>
2017-08-01 10:45:50 +0100

    feature(documentation): document API with slate
    Starts #149800711

--- server/route.js
+++ server/route.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 
@@ -10,6 +11,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Require our routes into the application.
 require('./routes')(app);
 
+app.use(express.static(path.resolve(__dirname, './../public')));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.resolve(__dirname, './../public', 'index.html'));
+});
 app.get('*', (req, res) => res.status(404).send({
   message: 'INVALID ROUTE!!!.',
 }));
